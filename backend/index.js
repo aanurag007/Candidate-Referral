22adeac2dfd7e2e000bda88884d171c1269839db
anurag yadav <90891980+aanurag007@users.noreply.github.com>
2025-02-01 10:14:47 +0530

    Update and rename server.js to index.js

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const candidateRoutes = require('./routes/candidateRoutes');
 const cors = require('cors');
+const { vercel } = require("vercel");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is working!");
+});
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 // Logging middleware
@@ -21,3 +25,5 @@ app.use('/api', candidateRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = vercel(app);
